15441041b50875e8fb49d7a40a0c604dab28efcb
Abraham Montes-Humanez <83221758+acmontesh@users.noreply.github.com>
2023-01-04 15:10:01 -0600

    updating curriculum page

--- views/paginas/curriculum.php
+++ views/paginas/curriculum.php
@@ -1,3 +1,18 @@
 <main>
     <h1>Curriculum Vitae</h1>
+    <p>This page is currently being updated. In the meantime, please find my CV at: </p>
+    <a href="https://www.linkedin.com/in/abraham-montes-humanez-6661a841/">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="56" height="56" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ff9300" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="16" height="16" rx="2" />
+            <line x1="8" y1="11" x2="8" y2="16" />
+            <line x1="8" y1="8" x2="8" y2="8.01" />
+            <line x1="12" y1="16" x2="12" y2="11" />
+            <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+        </svg>
+    </a>
+
+    <div class="goback">
+        <a href="/" class="boton", id="back">Go Back</a>
+    </div>
 </main>
